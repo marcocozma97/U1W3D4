47aa63d72c2aceed7d3f044fdc69546d89668322
Marco Cozma <marcocozma97@gmail.com>
2026-05-21 09:56:35 +0200

    calendar aggiornamento 2

---
+++ lezione/js/lezione.js
@@ -0,0 +1,30 @@
+const now = new Date();
+const getYear = now.getFullYear();
+const getMonth = now.getMonth();
+
+console.log(now);
+
+const monthNames = [
+    'Gennaio',
+    'Febbraio',
+    'Marzo',
+    'Aprile',
+    'Maggio',
+    'Giugno',
+    'Luglio',
+    'Agosto',
+    'Settembre',
+    'Ottobre',
+    'Novembre',
+    'Dicembre'
+];
+
+const dayNames = [
+    'Domenica',
+    'Lunedì',
+    'Martedì',
+    'Mercoledì',
+    'Giovedì',
+    'Venerdì',
+    'Sabato'
+];
